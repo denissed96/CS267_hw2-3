d50a4ba3e3df8649b788207b800c7b6b9ac22496
Qianqian <xpsqq@g.ucla.edu>
2022-03-14 15:22:03 -0700

    test compute force

--- gpu.cu
+++ gpu.cu
@@ -26,18 +26,18 @@ __global__ void print_part(particle_t const* parts, int num_parts) {
 }
 
 // print part ids in each bin, for debugging
-// static void print_bins(int* part_id, int* bin_id, int Nbin, int num_parts) {
-//    for (int i = 0; i < Nbin * Nbin; ++i) {
-//        int row = i / Nbin;
-//        int col = i % Nbin;
-//        std::cout << "Bin (" << row << ", " << col << "): ";
-//        int end = (i == Nbin * Nbin - 1) ? num_parts : bin_id[i + 1];
-//        for (int j = bin_id[i]; j < end; ++j) {
-//            std::cout << part_id[j] << ", ";
-//        }
-//        std::cout << std::endl;
-//    }
-//}
+void print_bins(int* part_id, int* bin_cnt, int Nbin, int num_parts) {
+    for (int i = 0; i < Nbin * Nbin; ++i) {
+        int row = i / Nbin;
+        int col = i % Nbin;
+        std::cout << "Bin (" << row << ", " << col << "): ";
+        int end = (i == Nbin * Nbin - 1) ? num_parts : bin_cnt[i + 1];
+        for (int j = bin_cnt[i]; j < end; ++j) {
+            std::cout << part_id[j] << ", ";
+        }
+        std::cout << std::endl;
+    }
+}
 
 // print details of parts in each bin, for debugging
 // static void print_parts_in_bins(PartBinsPerProc const& bins, int Nbin_col, int Nbin_row) {
@@ -51,65 +51,6 @@ __global__ void print_part(particle_t const* parts, int num_parts) {
 //        }
 //    }
 //}
-
-__device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
-    double dx = neighbor.x - particle.x;
-    double dy = neighbor.y - particle.y;
-    double r2 = dx * dx + dy * dy;
-    if (r2 > cutoff * cutoff)
-        return;
-    // r2 = fmax( r2, min_r*min_r );
-    r2 = (r2 > min_r * min_r) ? r2 : min_r * min_r;
-    double r = sqrt(r2);
-
-    //
-    //  very simple short-range repulsive force
-    //
-    double coef = (1 - cutoff / r) / r2 / mass;
-    particle.ax += coef * dx;
-    particle.ay += coef * dy;
-}
-
-__global__ void compute_forces_gpu(particle_t* particles, int num_parts) {
-    // Get thread (particle) ID
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid >= num_parts)
-        return;
-
-    particles[tid].ax = particles[tid].ay = 0;
-    for (int j = 0; j < num_parts; j++)
-        apply_force_gpu(particles[tid], particles[j]);
-}
-
-__global__ void move_gpu(particle_t* particles, int num_parts, double size) {
-
-    // Get thread (particle) ID
-    int tid = threadIdx.x + blockIdx.x * blockDim.x;
-    if (tid >= num_parts)
-        return;
-
-    particle_t* p = &particles[tid];
-    //
-    //  slightly simplified Velocity Verlet integration
-    //  conserves energy better than explicit Euler method
-    //
-    p->vx += p->ax * dt;
-    p->vy += p->ay * dt;
-    p->x += p->vx * dt;
-    p->y += p->vy * dt;
-
-    //
-    //  bounce from walls
-    //
-    while (p->x < 0 || p->x > size) {
-        p->x = p->x < 0 ? -(p->x) : 2 * size - p->x;
-        p->vx = -(p->vx);
-    }
-    while (p->y < 0 || p->y > size) {
-        p->y = p->y < 0 ? -(p->y) : 2 * size - p->y;
-        p->vy = -(p->vy);
-    }
-}
 
 __device__ int get_bin_idx(particle_t const& particle, int Nbin, double bin_size) {
     int idx_x = (int)(particle.x / bin_size);
@@ -133,7 +74,7 @@ __global__ void fill_bin_cnt(particle_t const* parts, int* bin_cnt, int num_part
     if (tid >= num_parts)
         return;
     int idx = get_bin_idx(parts[tid], Nbin, bin_size);
-    printf("Particle %d is in bin %d\n", tid, idx);
+    //    printf("Particle %d is in bin %d\n", tid, idx);
     atomicAdd(&bin_cnt[idx], 1);
 }
 
@@ -186,19 +127,104 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
     // copy bin_cnt to GPU again since the data on GPU are modified
     cudaMemcpy(bin_cnt_gpu, bin_cnt, Nbin * Nbin * sizeof(int), cudaMemcpyHostToDevice);
 
-    cudaMemcpy(part_id, part_id_gpu, num_parts * sizeof(int), cudaMemcpyDeviceToHost);
-    for (int i = 0; i < num_parts; ++i) {
-        std::cout << part_id[i] << " ";
-    }
-    std::cout << std::endl;
-
-    //    for (int i = 0; i < Nbin * Nbin; ++i) {
-    //        std::cout << bin_cnt[i] << " ";
-    //    }
-    //    std::cout << std::endl;
+    //    cudaMemcpy(part_id, part_id_gpu, num_parts * sizeof(int), cudaMemcpyDeviceToHost);
+    //    print_bins(part_id, bin_cnt, Nbin, num_parts);
 
     //    print_part<<<blks, NUM_THREADS>>>(parts, num_parts);
     //    cudaDeviceSynchronize();
+}
+
+__device__ bool check_cutoff_boundary(particle_t& particle, double bin_xx, double bin_yy,
+                                      double bin_size) {
+    return (particle.x - bin_xx > cutoff) && (bin_xx + bin_size - particle.x > cutoff) &&
+           (particle.y - bin_yy > cutoff) && (bin_yy + bin_size - particle.y > cutoff);
+}
+
+__device__ int get_neigh_cnt(int* bin_cnt, int idx, int Nbin, int num_parts) {
+    int row = idx / Nbin;
+    int col = idx % Nbin;
+    int row_start = (row - 1 >= 0) ? row - 1 : 0;
+    int row_end = (row + 1 < Nbin) ? row + 1 : Nbin - 1;
+    int col_start = (col - 1 >= 0) ? col - 1 : 0;
+    int col_end = (col + 1 < Nbin) ? col + 1 : Nbin - 1;
+    int tot_neigh_cnt = 0;
+    for (int i = row_start; i <= row_end; ++i) {
+        for (int j = col_start; j <= col_end; ++j) {
+            int bin_idx = i * Nbin + j;
+            int end = (bin_idx == Nbin * Nbin - 1) ? num_parts : bin_cnt[bin_idx + 1];
+            tot_neigh_cnt += (end - bin_cnt[bin_idx]);
+        }
+    }
+    return tot_neigh_cnt;
+}
+
+__device__ void apply_force_gpu(particle_t& particle, particle_t& neighbor) {
+    double dx = neighbor.x - particle.x;
+    double dy = neighbor.y - particle.y;
+    double r2 = dx * dx + dy * dy;
+    if (r2 > cutoff * cutoff)
+        return;
+    // r2 = fmax( r2, min_r*min_r );
+    r2 = (r2 > min_r * min_r) ? r2 : min_r * min_r;
+    double r = sqrt(r2);
+
+    //
+    //  very simple short-range repulsive force
+    //
+    double coef = (1 - cutoff / r) / r2 / mass;
+    particle.ax += coef * dx;
+    particle.ay += coef * dy;
+}
+
+__global__ void compute_forces_gpu(particle_t* parts, int* bin_cnt, int num_parts, int Nbin,
+                                   double bin_size) {
+    // Get thread (particle) ID
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    if (tid >= num_parts)
+        return;
+
+    parts[tid].ax = parts[tid].ay = 0;
+    // get bin idx for the particle
+    int bin_idx = get_bin_idx(parts[tid], Nbin, bin_size);
+    // get the number of neighbor parts
+    int neigh_cnt = get_neigh_cnt(bin_cnt, bin_idx, Nbin, num_parts);
+
+    int* neigh = new int[neigh_cnt];
+
+    for (int j = 0; j < num_parts; j++)
+        apply_force_gpu(parts[tid], parts[j]);
+
+    delete[] neigh;
+}
+
+__global__ void move_gpu(particle_t* particles, int num_parts, double size) {
+
+    // Get thread (particle) ID
+    int tid = threadIdx.x + blockIdx.x * blockDim.x;
+    if (tid >= num_parts)
+        return;
+
+    particle_t* p = &particles[tid];
+    //
+    //  slightly simplified Velocity Verlet integration
+    //  conserves energy better than explicit Euler method
+    //
+    p->vx += p->ax * dt;
+    p->vy += p->ay * dt;
+    p->x += p->vx * dt;
+    p->y += p->vy * dt;
+
+    //
+    //  bounce from walls
+    //
+    while (p->x < 0 || p->x > size) {
+        p->x = p->x < 0 ? -(p->x) : 2 * size - p->x;
+        p->vx = -(p->vx);
+    }
+    while (p->y < 0 || p->y > size) {
+        p->y = p->y < 0 ? -(p->y) : 2 * size - p->y;
+        p->vy = -(p->vy);
+    }
 }
 
 void simulate_one_step(particle_t* parts, int num_parts, double size) {
@@ -206,7 +232,7 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     // Rewrite this function
 
     // Compute forces
-    compute_forces_gpu<<<blks, NUM_THREADS>>>(parts, num_parts);
+    compute_forces_gpu<<<blks, NUM_THREADS>>>(parts, bin_cnt_gpu, num_parts, Nbin, bin_size);
 
     // Move particles
     move_gpu<<<blks, NUM_THREADS>>>(parts, num_parts, size);
